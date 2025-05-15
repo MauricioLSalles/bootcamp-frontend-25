ea972c36bf1c93cd3f9fe576d26b46800f638eae
MauricioLsalles <mauricio.leano@jala.university>
2025-05-15 08:19:01 -0400

    dynamic and weakly typed example

---
+++ Example2.js
@@ -0,0 +1,19 @@
+var item1;
+let item2 = null;
+let item3 = [];
+let item4 = 0;
+
+let bool = item1 == item2;
+let bool2 = item3 == item4;
+
+
+console.log(bool)
+console.log(bool2)
+
+bool = bool + bool
+
+console.log(bool)
+
+bool = bool + "2"
+
+console.log(bool)
