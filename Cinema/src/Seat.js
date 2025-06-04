1e62a7bdccb6c73f1270a3f81935e61db2a0e6f3
MauricioLsalles <mauricio.leano@jala.university>
2025-06-04 15:53:34 -0400

    seat class

---
+++ Cinema/src/Seat.js
@@ -0,0 +1,13 @@
+class Seat{
+    state = "empty";
+    row;
+    number;
+    constructor(row, number){   
+        this.row = row;
+        this.number = number;
+    }
+
+    toString() {
+    return this.state;
+  }
+}
