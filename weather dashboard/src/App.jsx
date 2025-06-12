86a2145fcf650c76331d20016204fe66934db53b
MauricioLsalles <mauricio.leano@jala.university>
2025-06-12 16:49:58 -0400

    create Climate search

--- weather dashboard/src/App.jsx
+++ weather dashboard/src/App.jsx
@@ -1,28 +1,11 @@
-import { useState } from 'react'
+import Climate from "./components/Climate"
 
 function App() {
-  const mockWeatherData = {
-    'New York': {
-      temperature: '22°C',
-      humidity: '56%',
-      windSpeed: '15 km/h'
-    },
-    'Los Angeles': {
-      temperature: '27°C',
-      humidity: '45%',
-      windSpeed: '10 km/h',
-    },
-    'London': {
-      temperature: '15°C',
-      humidity: '70%',
-      windSpeed: '20 km/h'
-    },
-  };
- 
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
+    <Climate/>
       </>
   )
 }
---
+++ weather dashboard/src/components/Climate.jsx
@@ -0,0 +1,29 @@
+function Climate() {
+    const mockWeatherData = {
+    'New York': {
+      temperature: '22°C',
+      humidity: '56%',
+      windSpeed: '15 km/h'
+    },
+    'Los Angeles': {
+      temperature: '27°C',
+      humidity: '45%',
+      windSpeed: '10 km/h',
+    },
+    'London': {
+      temperature: '15°C',
+      humidity: '70%',
+      windSpeed: '20 km/h'
+    },
+  };
+ 
+  return (
+    <div>
+        <input/>
+        <button>Search</button>
+        <button><b>Clean</b></button>
+    </div>
+  )
+}
+
+export default Climate
