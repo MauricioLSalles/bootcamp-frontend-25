c2fe940163b25a4ff43284b48fcc082b12d20685
MauricioLsalles <mauricio.leano@jala.university>
2025-06-19 16:36:53 -0400

    created list organism

---
+++ todo-list/src/components/organisms/List.jsx
@@ -0,0 +1,12 @@
+import ListItem from "../molecules/ListItem";
+
+function List(props) {
+const {items:items,...organismProps} = props;
+  return (
+    <ul {...organismProps}>{
+        items.map((item)=><ListItem text={item.text} clicked={item.clicked} /> )
+    }</ul>
+  )
+}
+
+export default List
