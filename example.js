bc4e0e09303b392117b9c6131137b5a0d152546f
MauricioLsalles <mauricio.leano@jala.university>
2025-07-11 16:47:54 -0400

    better example

--- example.js
+++ example.js
@@ -1,23 +1,32 @@
-console.log("start");
+console.log("start"); // executes 1
 
-setTimeout(() => console.log("timeOut"), 0);
+setTimeout(() => console.log("timeOut"), 0); //send to macrotask queue when resolved 8
 
 Promise.resolve().then(() => {
   console.log("promise");
-});
+}); //send to microtask queue 5
 
-console.log("middle");
+console.log("middle"); // executes 2
 
-long();
+long(); // call function
 
-for (let i = 0; i < 10000; i++) {}
+for (let i = 0; i < 10000; i++) {} // executes
 
-console.log("end");
+console.log("end"); // executes 4
 
 async function long() {
-  console.log("start of function");
+  console.log("start of function"); // executes 3
   await Promise.resolve().then(() => {
     console.log("middle of function");
-  });
-  console.log("end of function");
+  }); //send to microtask queue 6
+  console.log("end of function"); // executes once await is cleared 7
 }
+
+// start
+// middle
+// start of function
+// end
+// promise
+// middle of function
+// end of function
+// timeOut
