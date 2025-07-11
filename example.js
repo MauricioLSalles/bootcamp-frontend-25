6dbab94268a1f15a8564af16be037fcf82a218d9
MauricioLsalles <mauricio.leano@jala.university>
2025-07-11 16:44:35 -0400

    better example

--- example.js
+++ example.js
@@ -8,6 +8,16 @@ Promise.resolve().then(() => {
 
 console.log("middle");
 
+long();
+
 for (let i = 0; i < 10000; i++) {}
 
 console.log("end");
+
+async function long() {
+  console.log("start of function");
+  await Promise.resolve().then(() => {
+    console.log("middle of function");
+  });
+  console.log("end of function");
+}
